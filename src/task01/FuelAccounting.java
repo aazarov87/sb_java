fd4b9afdc8416ef4f1fa7963df5a5642393c1daa
aazarov87 <aazarov87>
2021-01-15 00:07:23 +0700

    refactor

--- src/task01/FuelAccounting.java
+++ src/task01/FuelAccounting.java
@@ -20,7 +20,7 @@ public class FuelAccounting {
             return 1;
     }
 // стоимость литра
-    public static double getCoast(String code){
+    public static double getPrice(String code){
         if (code.equals("100"))
             return 46.10;
         else if (code.equals("300"))
@@ -33,23 +33,23 @@ public class FuelAccounting {
 
     public static void main(String[] args) throws IOException {
 
-        double allCastConsumption = 0;
-        double CastConsumption;
+        double allCostConsumption = 0;
+        double CostConsumption;
         int idxArray = 0;
 
-        String codeMaxCoast = null;
-        String codeMinCoast = null;
-        double valMaxCoast = 0;
-        double valMinCoast = 0;
+        String codeMaxCost = null;
+        String codeMinCost = null;
+        double valMaxCost = 0;
+        double valMinCost = 0;
 
         //C(CODE_CAR)_гос номер-Пробег-(доп. параметр)
         String[] InData  = new String[] {"C100_1-100", "C200_1-120-1200", "C300_1-120-30", "C400_1-80-20", "C100_2-50", "C200_2-40-1000", "C300_2-200-45", "C400_2-10-20", "C100_3-10", "C200_3-170-1100", "C300_3-150-29", "C400_3-100-28", "C100_1-300", "C200_1-100-750", "C300_1-32-15"};
-        String[][] twoDimData = new String[InData.length][5];
-        double[] CastCons = new double[4];
+        String[][] twoDimData = new String[InData.length][5]; // массив данных в разрезе каждого ТС
+        double[] CastCons = new double[4]; // данные по затратам в разрезе типа ТС
 
         for (String data : InData) {
             int idx;
-            int idx1;
+            int idxParam;
 
             idx = data.indexOf("-");
             if (idx > -1) {
@@ -59,31 +59,31 @@ public class FuelAccounting {
                 twoDimData[idxArray][0] = codeCar; // код типа ТС
                 twoDimData[idxArray][1] = data.substring(5, idx); // номер
 
-                idx1 = data.indexOf("-", idx + 1);
+                idxParam = data.indexOf("-", idx + 1);
 
                 // пробег
-                if (idx1 > -1) {
+                if (idxParam > -1) {
 
                     twoDimData[idxArray][2] = data.substring(idx + 1, data.indexOf("-", idx + 1)); //пробег
-                    twoDimData[idxArray][3] = data.substring(idx1 + 1, data.length()); // доп параметр
+                    twoDimData[idxArray][3] = data.substring(idxParam + 1); // доп параметр
 
                 } else {
-                    twoDimData[idxArray][2] = data.substring(idx + 1, data.length());
+                    twoDimData[idxArray][2] = data.substring(idx + 1);
                 }
 
-                CastConsumption = Integer.parseInt(twoDimData[idxArray][2]) * getConsumption(codeCar) / 100 * getCoast(codeCar);
+                CostConsumption = Integer.parseInt(twoDimData[idxArray][2]) * getConsumption(codeCar) / 100 * getPrice(codeCar);
                 
                 if (codeCar.equals("100")){
-                    CastCons[0] += CastConsumption;
+                    CastCons[0] += CostConsumption;
                 }
                 else if (codeCar.equals("200")) {
-                    CastCons[1] += CastConsumption;
+                    CastCons[1] += CostConsumption;
                 }
                 else if (codeCar.equals("300")) {
-                    CastCons[2] += CastConsumption;
+                    CastCons[2] += CostConsumption;
                 }
                 else if (codeCar.equals("400")) {
-                    CastCons[3] += CastConsumption;
+                    CastCons[3] += CostConsumption;
                 }
 
                 idxArray += 1;
@@ -91,35 +91,37 @@ public class FuelAccounting {
 
         }
 
+        // общая стоимость + расход по каждому типу ТС
         for (int i = 0; i < CastCons.length; i++) {
             String codeT = i+1+"00";
             System.out.println("Расход для "+ codeT +" типа ТС "+ CastCons[i]);
-            allCastConsumption += CastCons[i];
+            allCostConsumption += CastCons[i];
 
             if (i == 0) {
-                valMaxCoast = CastCons[i];
-                valMinCoast = CastCons[i];
-                codeMaxCoast = codeT;
-                codeMinCoast = codeT;
+                valMaxCost = CastCons[i];
+                valMinCost = CastCons[i];
+                codeMaxCost = codeT;
+                codeMinCost = codeT;
             }
             else {
-                if (CastCons[i] > valMaxCoast) {
-                    valMaxCoast = CastCons[i];
-                    codeMaxCoast = codeT;
+                if (CastCons[i] > valMaxCost) {
+                    valMaxCost = CastCons[i];
+                    codeMaxCost = codeT;
                 }
-                if (CastCons[i] < valMinCoast) {
-                    valMinCoast = CastCons[i];
-                    codeMinCoast = codeT;
+                if (CastCons[i] < valMinCost) {
+                    valMinCost = CastCons[i];
+                    codeMinCost = codeT;
                 }
             }
         }
 
-        System.out.println("Общая стоимость расходов "+ allCastConsumption);
-        System.out.println("Наибольший расход у ТС типа "+ codeMaxCoast);
-        System.out.println("Наименьший расход у ТС типа "+ codeMinCoast);
+        System.out.println("Общая стоимость расходов "+ allCostConsumption);
+        System.out.println("Наибольший расход у ТС типа "+ codeMaxCost);
+        System.out.println("Наименьший расход у ТС типа "+ codeMinCost);
 
         System.out.println("----------------------");
 
+        // sort
         for (int out = twoDimData.length-1; out >= 0; out--){
             for (int in = 0; in < out; in++){
                 if (
@@ -137,6 +139,7 @@ public class FuelAccounting {
             }
         }
 
+        // вывод данных по запросу
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         while (true)
         {
