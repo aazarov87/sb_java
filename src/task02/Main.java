5439fa7207d1a1d64f2f4dc404cd92d6be9e2f7b
aazarov87 <aazarov87>
2021-01-18 11:08:12 +0700

    task02

--- src/task02/Main.java
+++ src/task02/Main.java
@@ -45,49 +45,37 @@ public class Main {
             if (s.equals("exit"))
                 break;
 
-            if (s.equals("1")) {
-                System.out.println("Введите значение:");
-                arrayList.addElement(Integer.parseInt(buffer.readLine()));
-            }
-            else if (s.equals("2")){
-                System.out.println("Введите индекс:");
-                idx = Integer.parseInt(buffer.readLine());
-
-                System.out.println("Введите значение:");
-                arrayList.addElement(idx, Integer.parseInt(buffer.readLine()));
-            }
-            else if (s.equals("3")){
-                arrayList.PrintSize();
-            }
-            else if (s.equals("4")){
-                System.out.println("Введите индекс:");
-                arrayList.removeElement(Integer.parseInt(buffer.readLine()));
-            }
-            else if (s.equals("5")){
-                System.out.println("Введите индекс:");
-                idx = Integer.parseInt(buffer.readLine());
-
-                System.out.println("Введите значение:");
-                arrayList.setElement(idx, Integer.parseInt(buffer.readLine()));
-            }
-            else if (s.equals("6")){
-                arrayList.Print();
-            }
-            else if (s.equals("7")){
-                arrayList.Sort();
-            }
-            else if (s.equals("8")){
-                arrayList.SortReverce();
-            }
-            else if (s.equals("9")){
-                arrayList.maxElement();
-            }
-            else if (s.equals("10")){
-                arrayList.minElement();
-            }
-            else if (s.equals("11")){
-                System.out.println("Введите значение:");
-                arrayList.setAllElement(Integer.parseInt(buffer.readLine()));
+            switch (s) {
+                case "1" -> {
+                    System.out.println("Введите значение:");
+                    arrayList.addElement(Integer.parseInt(buffer.readLine()));
+                }
+                case "2" -> {
+                    System.out.println("Введите индекс:");
+                    idx = Integer.parseInt(buffer.readLine());
+                    System.out.println("Введите значение:");
+                    arrayList.addElement(idx, Integer.parseInt(buffer.readLine()));
+                }
+                case "3" -> arrayList.PrintSize();
+                case "4" -> {
+                    System.out.println("Введите индекс:");
+                    arrayList.removeElement(Integer.parseInt(buffer.readLine()));
+                }
+                case "5" -> {
+                    System.out.println("Введите индекс:");
+                    idx = Integer.parseInt(buffer.readLine());
+                    System.out.println("Введите значение:");
+                    arrayList.setElement(idx, Integer.parseInt(buffer.readLine()));
+                }
+                case "6" -> arrayList.Print();
+                case "7" -> arrayList.Sort();
+                case "8" -> arrayList.SortReverce();
+                case "9" -> arrayList.maxElement();
+                case "10" -> arrayList.minElement();
+                case "11" -> {
+                    System.out.println("Введите значение:");
+                    arrayList.setAllElement(Integer.parseInt(buffer.readLine()));
+                }
             }
         }
     }
